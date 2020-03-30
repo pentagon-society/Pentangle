aea2ea10a9eaaed413c0c8581c2f66036617d807
Pentagrade <pentagrade@outlook.com>
2020-03-30 17:40:47 +0100

    Init project and gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pentangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
